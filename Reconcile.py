7f2f8146c40ec0b1b79484c0da97ea32268048e6
Feri-D <61537373+Feri-D@users.noreply.github.com>
2020-07-08 08:04:43 +0200

    parsing basics

--- Reconcile.py
+++ Reconcile.py
@@ -1,4 +1,4 @@
-#import csv
+import csv
 import os
 import tkinter as tk
 from tkinter import filedialog
@@ -19,9 +19,42 @@ with open(oldList, 'r') as t1, open(newList, 'r') as t2:
     filetwo = t2.readlines()
 print(fileone)
 print(filetwo)
+
 with open('Result.csv', 'w') as outFile:
     for line in filetwo:
         if line in fileone:
             outFile.write(line)
+            outFile.write("test \n")
+"""
+with open(oldList) as f:
+    for i, line in enumerate(f):             
+        print ("line {0} = {1}".format(i, line.split()))
+with open(newList) as f:
+    for i, line in enumerate(f):             
+        print("line {0} = {1}".format(i, line.split()))
+"""
+with open(oldList, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["ï»¿Name"]} costs {row["Price"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
 
-#os.startfile ('Result.csv')
+with open(newList, mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["ï»¿Name"]} costs {row["Price"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
+
+# Open result file
+# os.startfile ('Result.csv')
