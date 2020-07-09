8c407887fe75022a8e1ccc7caa4ae24c30651804
Feri-D <61537373+Feri-D@users.noreply.github.com>
2020-07-10 00:25:56 +0200

    fully working

--- Reconcile.py
+++ Reconcile.py
@@ -2,7 +2,7 @@ import csv
 import os
 import tkinter as tk
 from tkinter import filedialog
-"""
+
 # Ask for input files
 root = tk.Tk()
 root.withdraw()
@@ -13,30 +13,53 @@ newList = filedialog.askopenfilename(title='Select the NEW list')
 # Testing input file names
 oldList = 'Old_prices.csv'
 newList = 'New_prices.csv'
-
-with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('Result.csv', 'w') as outFile:
+"""
+# Open up files
+with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('Result.csv', 'w') as result_file:
     csv_old = csv.DictReader(old_file)
     csv_new = csv.DictReader(new_file)
-    oldLineCount = 0
-    newLineCount = 0
+    firstLine = False
 
+    # Evaulationg price differences
     for row_old in csv_old:  
-        if oldLineCount == 0:
+        found = False
+        if firstLine != True:
             oldLabels = csv_old.fieldnames
             newLabels = csv_new.fieldnames
-            outFile.write( f'{oldLabels[0]},{oldLabels[1]}')
-            oldLineCount += 1
-        oldLineCount += 1
-        newLineCount = 1
+            result_file.write( f'{newLabels[0]},{newLabels[1]}')
+            firstLine = True
         new_file.seek(0,0)
         for row_new in csv_new:
             if row_old[oldLabels[0]] == row_new[newLabels[0]]:
+                found = True
                 if row_old[oldLabels[1]] != row_new[newLabels[1]]:
-                    outFile.write( f'\n{row_old[oldLabels[0]]},{int(row_new[newLabels[1]])-int(row_old[oldLabels[1]])}')
-                    newLineCount += 1
-                newLineCount += 1
-            newLineCount += 1   
-    oldLineCount += 1
+                    result_file.write( f'\n{row_old[oldLabels[0]]},{int(row_new[newLabels[1]])-int(row_old[oldLabels[1]])}')
+                    print("difference saved",row_old[oldLabels[0]])
+                else:                 
+                    print("no diff found",row_old[oldLabels[0]])  
+                           
+        # Evaulationg removals
+        if found != True: 
+            result_file.write( f'\n{row_old[oldLabels[0]]},Removed')
+            print("not found",row_old[oldLabels[0]])   
 
-# Open result file
-#os.startfile ('Result.csv')
+    # Finding additions
+    new_file.seek(0,0)
+    for row_new in csv_new:
+        found = False
+        old_file.seek(0,0)
+        for row_old in csv_old:
+            if row_old[oldLabels[0]] == row_new[newLabels[0]]:
+                found = True
+    # log additions
+        if found != True: 
+            result_file.write( f'\n{row_new[newLabels[0]]},Added')
+            print("found as extra",row_new[newLabels[0]])   
+
+# Closing files
+old_file.close()
+new_file.close()
+result_file.close()
+
+# Present result file
+os.startfile ('Result.csv')
