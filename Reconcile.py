40d5b0973c1c0b003e46fe684ad873cf1cbf3d20
Feri-D <61537373+Feri-D@users.noreply.github.com>
2020-07-10 01:03:49 +0200

    included some logging

--- Reconcile.py
+++ Reconcile.py
@@ -1,19 +1,28 @@
 import csv
 import os
+import logging
 import tkinter as tk
 from tkinter import filedialog
 
+#set logging format, level and location
+logging.basicConfig(
+    format='%(asctime)s: Line %(lineno)d: %(message)s',
+    level=logging.INFO,
+    filename = 'reconcile.log',
+    filemode = 'w')
+logging.info('start')  
+"""
+# Testing input file names
+oldList = 'Old_prices.csv'
+newList = 'New_prices.csv'
+"""
 # Ask for input files
 root = tk.Tk()
 root.withdraw()
 
 oldList = filedialog.askopenfilename(title='Select the OLD list')
 newList = filedialog.askopenfilename(title='Select the NEW list')
-"""
-# Testing input file names
-oldList = 'Old_prices.csv'
-newList = 'New_prices.csv'
-"""
+
 # Open up files
 with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('Result.csv', 'w') as result_file:
     csv_old = csv.DictReader(old_file)
@@ -34,14 +43,17 @@ with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('
                 found = True
                 if row_old[oldLabels[1]] != row_new[newLabels[1]]:
                     result_file.write( f'\n{row_old[oldLabels[0]]},{int(row_new[newLabels[1]])-int(row_old[oldLabels[1]])}')
-                    print("difference saved",row_old[oldLabels[0]])
+                    print("difference saved:",row_old[oldLabels[0]])
+                    logging.info(f'difference saved for: {row_old[oldLabels[0]]}')
                 else:                 
-                    print("no diff found",row_old[oldLabels[0]])  
+                    print("no difference found:",row_old[oldLabels[0]])
+                    logging.info(f'no diffrerence found for: {row_old[oldLabels[0]]}')
                            
         # Evaulationg removals
         if found != True: 
             result_file.write( f'\n{row_old[oldLabels[0]]},Removed')
-            print("not found",row_old[oldLabels[0]])   
+            print("no entry found:",row_old[oldLabels[0]])   
+            logging.info(f'no entry found for: {row_old[oldLabels[0]]}')
 
     # Finding additions
     new_file.seek(0,0)
@@ -54,7 +66,8 @@ with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('
     # log additions
         if found != True: 
             result_file.write( f'\n{row_new[newLabels[0]]},Added')
-            print("found as extra",row_new[newLabels[0]])   
+            print("found as extra:",row_new[newLabels[0]]) 
+            logging.info(f'new entry found for: {row_new[newLabels[0]]}')  
 
 # Closing files
 old_file.close()
@@ -63,3 +76,5 @@ result_file.close()
 
 # Present result file
 os.startfile ('Result.csv')
+
+logging.info('done')  
