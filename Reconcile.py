b9b3a9f13838880e5cdc3f16ce2a932f6ac78dba
Feri-D <61537373+Feri-D@users.noreply.github.com>
2020-07-09 18:45:49 +0200

    dictionary

--- Reconcile.py
+++ Reconcile.py
@@ -14,47 +14,19 @@ newList = filedialog.askopenfilename(title='Select the NEW list')
 oldList = 'Old_prices.csv'
 newList = 'New_prices.csv'
 
-with open(oldList, 'r') as t1, open(newList, 'r') as t2:
-    fileone = t1.readlines()
-    filetwo = t2.readlines()
-print(fileone)
-print(filetwo)
-
-with open('Result.csv', 'w') as outFile:
-    for line in filetwo:
-        if line in fileone:
-            outFile.write(line)
-            outFile.write("test \n")
-"""
-with open(oldList) as f:
-    for i, line in enumerate(f):             
-        print ("line {0} = {1}".format(i, line.split()))
-with open(newList) as f:
-    for i, line in enumerate(f):             
-        print("line {0} = {1}".format(i, line.split()))
-"""
-with open(oldList, mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('Result.csv', 'w') as outFile:
+    csv_old = csv.DictReader(old_file)
+    csv_new = csv.DictReader(new_file)
     line_count = 0
-    for row in csv_reader:
+    for row in csv_old:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            labels = csv_old.fieldnames
+            outFile.write( f'{labels[0]},{labels[1]}')
             line_count += 1
-        print(f'\t{row["ï»¿Name"]} costs {row["Price"]}.')
+        print(f'\t{row[labels[0]]} costs {row[labels[1]]}.')
+        outFile.write( f'\n{row[labels[0]]},{row[labels[1]]}')
         line_count += 1
     print(f'Processed {line_count} lines.')
-
-with open(newList, mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        print(f'\t{row["ï»¿Name"]} costs {row["Price"]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
-
 
 # Open result file
 # os.startfile ('Result.csv')
