2a6e7b98f340f13ab019ed6c09d96fd6ae700714
Feri-D <61537373+Feri-D@users.noreply.github.com>
2020-07-09 21:49:21 +0200

    pricecount

--- Reconcile.py
+++ Reconcile.py
@@ -17,16 +17,26 @@ newList = 'New_prices.csv'
 with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('Result.csv', 'w') as outFile:
     csv_old = csv.DictReader(old_file)
     csv_new = csv.DictReader(new_file)
-    line_count = 0
-    for row in csv_old:
-        if line_count == 0:
-            labels = csv_old.fieldnames
-            outFile.write( f'{labels[0]},{labels[1]}')
-            line_count += 1
-        print(f'\t{row[labels[0]]} costs {row[labels[1]]}.')
-        outFile.write( f'\n{row[labels[0]]},{row[labels[1]]}')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+    oldLineCount = 0
+    newLineCount = 0
+
+    for row_old in csv_old:  
+        if oldLineCount == 0:
+            oldLabels = csv_old.fieldnames
+            newLabels = csv_new.fieldnames
+            outFile.write( f'{oldLabels[0]},{oldLabels[1]}')
+            oldLineCount += 1
+        oldLineCount += 1
+        newLineCount = 1
+        new_file.seek(0,0)
+        for row_new in csv_new:
+            if row_old[oldLabels[0]] == row_new[newLabels[0]]:
+                if row_old[oldLabels[1]] != row_new[newLabels[1]]:
+                    outFile.write( f'\n{row_old[oldLabels[0]]},{int(row_new[newLabels[1]])-int(row_old[oldLabels[1]])}')
+                    newLineCount += 1
+                newLineCount += 1
+            newLineCount += 1   
+    oldLineCount += 1
 
 # Open result file
-# os.startfile ('Result.csv')
+#os.startfile ('Result.csv')
