57cdf5df0c9b802b0387ef6f7fdbcb4e10f01db8
Feri-D <61537373+Feri-D@users.noreply.github.com>
2020-07-10 02:50:36 +0200

    handle both command line and direct input

--- Reconcile.py
+++ Reconcile.py
@@ -1,27 +1,60 @@
-import csv
+import sys
 import os
+import csv
 import logging
 import tkinter as tk
 from tkinter import filedialog
 
-#set logging format, level and location
-logging.basicConfig(
-    format='%(asctime)s: Line %(lineno)d: %(message)s',
-    level=logging.INFO,
-    filename = 'reconcile.log',
-    filemode = 'w')
+# SELECT TEST MODE HERE
+TestMode = False
+
+# Set logging format, level and location
+if TestMode == True:
+    logging.basicConfig(
+        format='%(asctime)s:%(levelname)s:Line %(lineno)d: %(message)s',
+        level=logging.DEBUG,
+        filename = 'reconcile.log',
+        filemode = 'w')
+else:
+    logging.basicConfig(
+        format='%(asctime)s: Line %(lineno)d: %(message)s',
+        level=logging.INFO,
+        filename = 'reconcile.log',
+        filemode = 'w')
 logging.info('start')  
-"""
-# Testing input file names
-oldList = 'Old_prices.csv'
-newList = 'New_prices.csv'
-"""
-# Ask for input files
+logging.debug("TEST MODE")
+
+remoteUser = False
+oldList = None
+newList = None
+
+# Files used in test mode
+if TestMode == True:
+    oldList = 'Old_prices.csv'
+    newList = 'New_prices.csv'
+    logging.debug("TEST INPUTS USED")
+
+# Handle input file names as arguments
+if (len(sys.argv)>1 ):
+    oldList = sys.argv[1]
+    logging.debug("FIRST argument received")
+    print ("Usage details: Reconcile.py <OldListPath> <NewListPath>")
+if (len(sys.argv)>2 ):
+    newList = sys.argv[2]
+    logging.debug("SECOND argument received")    
+    remoteUser = True
+if (len(sys.argv)>3 ):
+    print ("Usage details: Reconcile.py <OldListPath> <NewListPath>")
+    logging.debug("TOO MANY arguments received")
+
+# Ask for missing input files
 root = tk.Tk()
 root.withdraw()
 
-oldList = filedialog.askopenfilename(title='Select the OLD list')
-newList = filedialog.askopenfilename(title='Select the NEW list')
+if oldList == None:
+    oldList = filedialog.askopenfilename(title='Select the OLD list')
+if newList == None:
+    newList = filedialog.askopenfilename(title='Select the NEW list')
 
 # Open up files
 with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('Result.csv', 'w') as result_file:
@@ -29,7 +62,7 @@ with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('
     csv_new = csv.DictReader(new_file)
     firstLine = False
 
-    # Evaulationg price differences
+# Evaluate price differences
     for row_old in csv_old:  
         found = False
         if firstLine != True:
@@ -49,13 +82,13 @@ with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('
                     print("no difference found:",row_old[oldLabels[0]])
                     logging.info(f'no diffrerence found for: {row_old[oldLabels[0]]}')
                            
-        # Evaulationg removals
+# Evaluate removals
         if found != True: 
             result_file.write( f'\n{row_old[oldLabels[0]]},Removed')
             print("no entry found:",row_old[oldLabels[0]])   
             logging.info(f'no entry found for: {row_old[oldLabels[0]]}')
 
-    # Finding additions
+# Find additions
     new_file.seek(0,0)
     for row_new in csv_new:
         found = False
@@ -63,18 +96,20 @@ with open(oldList, mode='r') as old_file, open(newList, 'r') as new_file, open('
         for row_old in csv_old:
             if row_old[oldLabels[0]] == row_new[newLabels[0]]:
                 found = True
-    # log additions
+# Log additions
         if found != True: 
             result_file.write( f'\n{row_new[newLabels[0]]},Added')
             print("found as extra:",row_new[newLabels[0]]) 
             logging.info(f'new entry found for: {row_new[newLabels[0]]}')  
 
-# Closing files
+# Close files
 old_file.close()
 new_file.close()
 result_file.close()
 
 # Present result file
-os.startfile ('Result.csv')
+if (TestMode != True)*(remoteUser != True):
+    os.startfile ('Result.csv')
+print("Saved in Result.csv")   
 
 logging.info('done')  
